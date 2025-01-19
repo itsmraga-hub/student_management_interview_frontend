33fdf7be3d0b3de780ca2c1c1dd2e988577c8d63
itsmraga-hub <itsragamit@gmail.com>
2025-01-19 22:46:02 +0300

    add photoPath response model

--- src/app/interfaces/student.ts
+++ src/app/interfaces/student.ts
@@ -10,7 +10,7 @@ export interface Student {
 }
 
 
-// export interface PhotoPath {
-//   photoUrl: string;
-//   message: string;
-// }
+export interface PhotoPath {
+  photoUrl: string;
+  message: string;
+}
