7641b52d722c99b9e46d16bf3c8a2de1c8d8dfa3
itsmraga-hub <itsragamit@gmail.com>
2025-01-19 22:49:20 +0300

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EditStudentComponent } from './edit-student/edit-student.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'edit-student/:id', component: EditStudentComponent },
+  // { path: '', component: HomeComponent },
+  // { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {path: '**', component: PageNotFoundComponent},
+  
+];
