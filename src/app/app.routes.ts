bea3e760561903849dcd8aea2cc083ed27e4e09b
itsmraga-hub <itsragamit@gmail.com>
2025-01-20 15:45:53 +0300

    Configure routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,13 +9,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { EditStudentComponent } from './edit-student/edit-student.component';
+import { authGuard } from './auth.guard';
+import { StudentsComponent } from './students/students.component';
+import { StudentComponent } from './student/student.component';
+import { GenerationComponent } from './generation/generation.component';
+import { ProcessingComponent } from './processing/processing.component';
+import { ReportComponent } from './report/report.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'edit-student/:id', component: EditStudentComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
+  { path: 'generate', component: GenerationComponent, canActivate: [authGuard] },
+  { path: 'processing', component: ProcessingComponent, canActivate: [authGuard] },
+  { path: 'report', component: ReportComponent, canActivate: [authGuard] },
+  { path: 'students', component: StudentsComponent, canActivate: [authGuard] },
+  { path: 'student/:id', component: StudentComponent, canActivate: [authGuard] },
+  // { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'students/edit/:id', component: EditStudentComponent, canActivate: [authGuard] },
   // { path: '', component: HomeComponent },
   // { path: '', redirectTo: '/login', pathMatch: 'full' },
   {path: '**', component: PageNotFoundComponent},
