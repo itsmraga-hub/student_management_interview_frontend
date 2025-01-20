29b198a71ba14c81e519168ba5000cb34c23a7d7
itsmraga-hub <itsragamit@gmail.com>
2025-01-20 15:44:58 +0300

    students - use tailwind

--- src/app/students/students.component.ts
+++ src/app/students/students.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
 import { Student } from '../interfaces/student'; // Import the model
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
@@ -10,31 +10,36 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
 import { MatSnackBar } from '@angular/material/snack-bar';
-
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-students',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule, HttpClientModule, MatDialogModule,
-    MatButtonModule],
+    MatButtonModule, FormsModule],
   templateUrl: './students.component.html',
   styleUrls: ['./students.component.css']
 })
 export class StudentsComponent implements OnInit {
-  students: Student[] = []; // Array to store students
+  students: Student[] = [];
   userString = localStorage.getItem('user');
   user = this.userString ? JSON.parse(this.userString) : null;
   headers = new HttpHeaders({
     'Content-Type': 'application/json',
-    'Authorization': `Bearer ${this.user.token}` // Pass the token in the Authorization header
+    'Authorization': `Bearer ${this.user.token}`
   });
 
   paginatedStudents: Student[] = [];
   currentPage: number = 1;
   pageSize: number = 10;
   totalPages: number = 0;
-
+  searchStudentId: string = '';
+  selectedClass: string = '';
+  startDate: string = '';
+  endDate: string = '';
+  fileName: string = '';
   
+  classes: string[] = ['Class 1', 'Class 2', 'Class 3', 'Class 4', 'Class 5']; 
 
   constructor(private http: HttpClient,
     private router: Router,
@@ -78,6 +83,16 @@ export class StudentsComponent implements OnInit {
       width: '300px'
     });
 
+    const userString = localStorage.getItem('user');
+    console.log('User string:', userString);
+    const user = userString ? JSON.parse(userString) : null;
+    console.log('User:', user);
+    console.log('User:', user.token);
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${user.token}` // Pass the token in the Authorization header
+    });
     // Call the API to delete the student
     // this.http.delete(`http://localhost:8080/api/students/${studentId}`).subscribe(() => {
     //   this.loadStudents(); // Reload the students after deletion
@@ -85,7 +100,7 @@ export class StudentsComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe(result => {
       if (result) {
-        this.http.delete(`http://localhost:8080/students/${studentId}`).subscribe({
+        this.http.delete(`http://localhost:8080/students/${studentId}`, {headers}).subscribe({
           next: () => {
             console.log(`Student with ID ${studentId} deleted`);
             this.students = this.students.filter(student => student.studentId !== studentId);
@@ -140,4 +155,58 @@ export class StudentsComponent implements OnInit {
       this.updatePagination();
     }
   }
+
+  applyFilters(): void {
+    let filteredStudents = [...this.students];
+
+    // Apply Student ID Filter
+    if (this.searchStudentId) {
+      filteredStudents = filteredStudents.filter(student =>
+        student.studentId.toString().includes(this.searchStudentId)
+      );
+    }
+
+    // Apply Class Filter
+    if (this.selectedClass) {
+      filteredStudents = filteredStudents.filter(student =>
+        student.studentClass === this.selectedClass
+      );
+    }
+
+    // Apply Date of Birth Filter
+    if (this.startDate && this.endDate) {
+      filteredStudents = filteredStudents.filter(student =>
+        new Date(student.dob) >= new Date(this.startDate) && new Date(student.dob) <= new Date(this.endDate)
+      );
+    }
+
+    // Paginate the filtered results
+    this.totalPages = Math.ceil(filteredStudents.length / 10);
+    this.paginatedStudents = filteredStudents.slice((this.currentPage - 1) * 10, this.currentPage * 10);
+  }
+
+  exportToExcel(): void {
+    const filteredData = this.paginatedStudents.map(student => ({
+      'StudentId': student.studentId,
+      'First Name': student.firstName,
+      'Last Name': student.lastName,
+      'Class': student.studentClass,
+      'Score': student.score,
+      'Photo Path': student.photoPath,
+      // 'Status': student.status,
+      'Date of Birth': student.dob,
+    }));
+    const fileName = this.fileName ? `${this.fileName}.xlsx` : 'students.xlsx';
+    // Create a worksheet from the filtered data
+    const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(filteredData);
+
+    // Create a workbook and add the worksheet
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Students');
+
+    // Export to file
+    XLSX.writeFile(wb, fileName);
+  }
+
+
 }
